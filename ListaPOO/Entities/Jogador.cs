f64ccc66a28ae1f5b6b673690ff49f318e7c4036
sthevanbello <sthevan.alves@gmail.com>
2022-07-27 14:43:42 -0300

    Exercício 6 - Implementado o método TrocarAcao para atualizar a ação (Enumerator) de acordo com a tecla apertada.

---
+++ ListaPOO/Entities/Jogador.cs
@@ -0,0 +1,55 @@
+﻿using ListaPOO.Enums;
+using ListaPOO.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPOO.Entities
+{
+    public class Jogador : IJogador
+    {
+        public string Nome { get; set; }
+        public float Vida { get; set; }
+        public EAcoes Acoes { get; set; }
+
+        public string Morrer()
+        {
+            Vida = 0;
+            return "---- Game Over ----";
+        }
+
+        public void TrocarAcao()
+        {
+            bool sair = false;
+            do
+            {
+                ConsoleKey key = Console.ReadKey(true).Key;
+                switch (key)
+                {
+                    case ConsoleKey.W:
+                        Acoes = EAcoes.Pulando;
+                        Console.WriteLine($"\nJogador está {Acoes}");
+                        break;
+                    case ConsoleKey.A:
+                        Acoes = EAcoes.Correndo;
+                        Console.WriteLine($"\nJogador está {Acoes}");
+                        break;
+                    case ConsoleKey.S:
+                        Acoes = EAcoes.Defendendo;
+                        Console.WriteLine($"\nJogador está {Acoes}");
+                        break;
+                    case ConsoleKey.D:
+                        Acoes = EAcoes.Atacando;
+                        Console.WriteLine($"\nJogador está {Acoes}");
+                        break;
+                    default:
+                        Console.WriteLine("\nJogador ficou sem saber o que fazer");
+                        sair = true;
+                        break;
+                }
+            } while (!sair);
+        }
+    }
+}
---
+++ ListaPOO/Enums/EAcoes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPOO.Enums
+{
+    public enum EAcoes
+    {
+        Atacando,
+        Defendendo,
+        Pulando,
+        Correndo
+    }
+}
