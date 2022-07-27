720ef7a6abc6222d9f6f75a4967569c278dafe75
sthevanbello <sthevan.alves@gmail.com>
2022-07-27 14:45:03 -0300

    Exercício 7 - Criada uma interface para Jogador com as propriedades e métodos originais e adicionado um método novo chamado Morrer.

---
+++ ListaPOO/Interfaces/IJogador.cs
@@ -0,0 +1,19 @@
+﻿using ListaPOO.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPOO.Interfaces
+{
+    public interface IJogador
+    {
+        EAcoes Acoes { get; set; }
+        string Nome { get; set; }
+        float Vida { get; set; }
+
+        void TrocarAcao();
+        string Morrer();
+    }
+}
